9cd3aecf40bb548ef73947a7b9e96ee962877195
thorcc <thorchrcow@gmail.com>
2019-06-04 10:03:50 +0200

    html og css

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Referanser til html-elementer
 const audio = document.querySelectorAll("audio");
+const divTrommer = document.querySelectorAll(".tromme");
 
 // Globale variabler
 const knapper = ["a","s","d","f","g","h","j","k","l"];
@@ -8,8 +9,15 @@ const knapper = ["a","s","d","f","g","h","j","k","l"];
 document.onkeydown = function(event){
   for(let i = 0;i < 9; i++){
     if(event.key ===knapper[i]){
+      divTrommer[i].classList.add("valgtTromme");
       audio[i].play();
       audio[i].currentTime = 0;
     }
+  }
+}
+
+document.onkeyup = function(event){
+  for(let i = 0; i < 9; i++){
+    divTrommer[i].classList.remove("valgtTromme");
   }
 }
